663e62b2dcfe09e126bea5d17ed3fed0d800402b
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2023-01-15 17:12:07 +0500

    Update StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,11 +3,6 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         if (str == null || str.trim().length() == 0 || str.equals("null")) return false;
-        
-        Double number=Double.parseDouble(str);
-
-        if (number<=0)return false;
-        return true;
-//        return Double.parseDouble(str) > 0;
+        return Double.parseDouble(str) > 0;
     }
 }
